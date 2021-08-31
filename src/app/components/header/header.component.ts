6a0a1fd8ff08769f51d97ebbbf31f678ef289c7b
Oogaga <45121623+Oogaga@users.noreply.github.com>
2021-08-31 09:58:21 +0300

    header.component.html firebase hosting

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
 // import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
@@ -9,9 +10,11 @@ import { Component, OnInit } from '@angular/core';
 
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+
   }
 
 
